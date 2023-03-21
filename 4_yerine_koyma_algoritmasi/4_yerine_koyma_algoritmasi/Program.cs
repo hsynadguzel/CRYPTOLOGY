6913b9ab59eb05a230a965e127752808e6a4c4d2
Huseyin <98910348+hsynadguzel@users.noreply.github.com>
2023-03-21 22:22:05 +0300

    C#

---
+++ 4_yerine_koyma_algoritmasi/4_yerine_koyma_algoritmasi/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_yerine_koyma_algoritmasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sifreleme sifreleme = new Sifreleme();
+            Console.WriteLine("Klasik Sifreleme / Yerine Koyma Algoritmasi");
+            Console.WriteLine("Sifrelenmis: " + sifreleme.yerineKoyma("ankaragücü")); // oijotodhrh
+            Console.WriteLine("Sifresi Cozulmus: " + sifreleme.yerineKoymaCozme("oijotodhrh")); // ankaragücü
+            Console.ReadKey();
+        }
+    }
+}
+
+class Sifreleme
+{
+    public List<string> turkceAlfabe = new List<string> {
+        "a",
+        "b",
+        "c",
+        "ç",
+        "d",
+        "e",
+        "f",
+        "g",
+        "ğ",
+        "h",
+        "ı",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "ö",
+        "p",
+        "r",
+        "s",
+        "ş",
+        "t",
+        "u",
+        "ü",
+        "v",
+        "y",
+        "z"
+      };
+
+    public List<string> randomAlfabe = new List<string> { "o", "ü", "r", "b", "ş", "ç", "m", "d", "k", "e", "f", "g", "ğ", "j", "ı", "n", "i", "p", "a", "ö",  "t", "s",  "v", "u", "c",  "h", "y", "z" };
+
+    public string yerineKoyma(string veri)
+    {
+        string sifrelenmisVeri = "";
+        char[] veriList = veri.ToCharArray();
+        for (int i = 0; i < veriList.Length; i++)
+        {
+            if (turkceAlfabe.Contains(veriList[i].ToString()))
+            {
+                int index = turkceAlfabe.IndexOf(veriList[i].ToString());
+                string indexA = randomAlfabe[index].ToString();
+                sifrelenmisVeri += indexA;
+                
+            }
+            else
+            {
+                sifrelenmisVeri += "*";
+            }
+
+        }
+        return sifrelenmisVeri;
+    }
+
+    public string yerineKoymaCozme(string veri)
+    {
+        string cozulmusVeri = "";
+        char[] veriList = veri.ToCharArray();
+        for (int i = 0; i < veriList.Length; i++)
+        {
+            if (randomAlfabe.Contains(veriList[i].ToString()))
+            {
+                int index = randomAlfabe.IndexOf(veriList[i].ToString());
+                string indexA = turkceAlfabe[index].ToString();
+                cozulmusVeri += indexA;
+            }
+            else
+            {
+                cozulmusVeri += "?";
+            }
+        }
+        return cozulmusVeri;
+    }
+
+}
---
+++ 5_permutasyon_sifreleme/5_permutasyon_sifreleme/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_permutasyon_sifreleme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sifreleme sifreleme = new Sifreleme();
+            Console.WriteLine("Klasik Sifreleme / Yerine Koyma Algoritmasi");
+            //Console.WriteLine("Sifrelenmis: " + sifreleme.yerineKoyma("ankaragücü")); // oijotodhrh
+            //Console.WriteLine("Sifresi Cozulmus: " + sifreleme.yerineKoymaCozme("oijotodhrh")); // ankaragücü
+            Console.ReadKey();
+        }
+    }
+}
+
+            
+class Sifreleme
+{
+    public List<string> turkceAlfabe = new List<string> {
+        "a",
+        "b",
+        "c",
+        "ç",
+        "d",
+        "e",
+        "f",
+        "g",
+        "ğ",
+        "h",
+        "ı",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "ö",
+        "p",
+        "r",
+        "s",
+        "ş",
+        "t",
+        "u",
+        "ü",
+        "v",
+        "y",
+        "z"
+      };
+
+    public string yerineKoyma(string veri, int anahtar)
+    {
+        string sifrelenmisVeri = "";
+        List<int> ints= new List<int>();
+        if (anahtar > 0 && anahtar <= veri.Length)
+        {
+            for (int i = 1; i <= anahtar; i++)
+            {
+                ints.Add(i);
+            }
+
+
+            char[] veriList = veri.ToCharArray();
+            for (int i = 0; i < veriList.Length; i++)
+            {
+                if (turkceAlfabe.Contains(veriList[i].ToString()))
+                {
+                    int index = turkceAlfabe.IndexOf(veriList[i].ToString());
+                    string indexA = randomAlfabe[index].ToString();
+                    sifrelenmisVeri += indexA;
+
+                }
+                else
+                {
+                    sifrelenmisVeri += "*";
+                }
+
+            }
+        }
+        else
+        {
+            sifrelenmisVeri = 'Anahtar Degeri Hatalı!';
+        }
+        
+        return sifrelenmisVeri;
+    }
+
+    public string yerineKoymaCozme(string veri)
+    {
+        string cozulmusVeri = "";
+        char[] veriList = veri.ToCharArray();
+        for (int i = 0; i < veriList.Length; i++)
+        {
+            if (randomAlfabe.Contains(veriList[i].ToString()))
+            {
+                int index = randomAlfabe.IndexOf(veriList[i].ToString());
+                string indexA = turkceAlfabe[index].ToString();
+                cozulmusVeri += indexA;
+            }
+            else
+            {
+                cozulmusVeri += "?";
+            }
+        }
+        return cozulmusVeri;
+    }
+
+}
